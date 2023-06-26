5ffd898f2bf4c40e5557863c9329d7a0531ff4d8
dbrach <brach@veloxalpha.com>
2023-06-26 17:51:50 +0200

    Project Setup

---
+++ CS_Backend/src/main/java/com/cs_backend/CsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsBackendApplication.class, args);
+    }
+
+}
---
+++ CS_Backend/src/test/java/com/cs_backend/CsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
