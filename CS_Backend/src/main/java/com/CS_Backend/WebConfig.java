54a732f4719647776d05a601bab9d383127b403c
dbrach <dawid.brach56@gmail.com>
2023-07-02 22:41:32 +0200

    CORS

--- CS_Backend/src/main/java/com/CS_Backend/WebConfig.java
+++ CS_Backend/src/main/java/com/CS_Backend/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://afraid-industry-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
